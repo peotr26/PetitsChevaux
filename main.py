2ae94be174d585841ebece7792243354b5ed3ad1
peotr26 <67782057+peotr26@users.noreply.github.com>
2023-01-26 16:09:40 +0100

    Almost finish

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from tkinter import *
-from time import sleep
 
 # Variables
 
 W = 400
 H = 400
 
+global taille_serpent
+#global etat[0]
+global taille_bloc
+
 etat = [6] # 4 = gauche, 6 = droite, 8 = haut, 2 = bas
-taille = [4]
+taille_serpent = 4
+taille_bloc= H/20
 
 serpent_base = [
-    [200, 200, 205, 205],
-    [205, 200, 210, 205],
-    [210, 200, 215, 205],
-    [215, 200, 220, 205]
+    [W/2+(taille_bloc*0), W/2, H/2+(taille_bloc*0), H/2],
+    [W/2+(taille_bloc*1), W/2, H/2+(taille_bloc*1), H/2],
+    [W/2+(taille_bloc*2), W/2, H/2+(taille_bloc*2), H/2],
+    [W/2+(taille_bloc*3), W/2, H/2+(taille_bloc*3), H/2]
 ]
 
 colour = [
     '#000000',  # Noir
     '#FFFFFF',  # Blanc
+    'green'
 ]
 
 # Fonctions
@@ -43,10 +48,16 @@ def gauche(event):
     if etat[0] == 8 or etat[0] == 2:
         etat[0] = 4
 
-def off_limit(x, y):
-    if x >= 400 or y >= 400:
+def hors_limite(x:int, y:int):
+    if x >= W-taille_bloc or y >= H-taille_bloc or x <= 0-taille_bloc or y <= 0-taille_bloc:
         game.delete(ALL)
         game.create_text(W/2-25, H/2-25, text='Game \n over')
+
+def suicide(x:int, y:int, serpent:list):
+    for i in range(0, int(taille_bloc)):
+        if serpent[i][0] < x < serpent[i][1] or serpent[i][2] < y < serpent[i][3]:
+            game.delete(ALL)
+            game.create_text(W/2-25, H/2-25, text='Game \n over')
 
 def new_game():
     game.delete(ALL)
@@ -57,26 +68,24 @@ def new_game():
 def deplacement():
     if etat[0] == 6:
         origine = list(serpent)
-        for i in range(0,taille[0]):
-            serpent[i] = [serpent[i][0]+20, serpent[i][1], serpent[i][2]+20, serpent[i][3]]
+        serpent[0] = [serpent[0][0]+taille_bloc, serpent[0][1], serpent[0][2]+taille_bloc, serpent[0][3]]
     if etat[0] == 4:
         origine = list(serpent)
-        for i in range(0,taille[0]):
-            serpent[i] = [serpent[i][0]-20, serpent[i][1], serpent[i][2]-20, serpent[i][3]]
+        serpent[0] = [serpent[0][0]-taille_bloc, serpent[0][1], serpent[0][2]-taille_bloc, serpent[0][3]]
     if etat[0] == 8:
         origine = list(serpent)
-        for i in range(0,taille[0]):
-            serpent[i] = [serpent[i][0], serpent[i][1]-20, serpent[i][2], serpent[i][3]-20]
+        serpent[0] = [serpent[0][0], serpent[0][1]-taille_bloc, serpent[0][2], serpent[0][3]-taille_bloc]
     if etat[0] == 2:
         origine = list(serpent)
-        for i in range(0,taille[0]):
-            serpent[i] = [serpent[i][0], serpent[i][1]+20, serpent[i][2], serpent[i][3]+20]
-    for i in range(0,taille[0]):
-        game.create_rectangle(serpent[i])
-    for i in range(0, taille[0]):
-        game.create_rectangle(origine[i], outline=colour[1])
-    off_limit(serpent[0][0], serpent[0][1])
-    game.after(200, deplacement)
+        serpent[0] = [serpent[0][0], serpent[0][1]+taille_bloc, serpent[0][2], serpent[0][3]+taille_bloc]
+    for i in range(1,taille_serpent):
+        serpent[i] = origine[i-1]
+    for i in range(0,taille_serpent):
+        game.create_rectangle(serpent[i], width=taille_bloc , outline=colour[2], fill=colour[2])
+    game.create_rectangle(origine[taille_serpent-1], width=taille_bloc, outline=colour[1], fill=colour[1])
+    hors_limite(serpent[0][0], serpent[0][1])
+    suicide(serpent[0][0], serpent[0][1], serpent)
+    game.after(100, deplacement)
 
 # Widgets
 
